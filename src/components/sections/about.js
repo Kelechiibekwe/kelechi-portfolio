a3575930ec49bb5def85916212a68cb5ab1a0aa2
Kelechi Ibekwe <kibekwe96@gmail.com>
2024-08-06 22:24:43 -0600

    Modified the about me section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Java', 'SpringBoot', 'Python', 'JavaScript', 'Node.js', 'Next.js'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,33 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hey! I’m Kelechi, a software developer from Canada who loves creating amazing digital
+              experiences. My journey into tech began with a Mechanical Engineering degree, where I
+              discovered my passion for problem-solving and computer science.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Determined to dive deeper, I earned a certification in computer science from MIT on
+              edX and leveled up my skills through a bootcamp, building projects that sharpened my
+              expertise in system design, data structures, and algorithms.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              Fast forward to today, I’m thriving at{' '}
+              <a href="https://www.td.com/ca/en/about-td"> TD Group</a>, where I’ve worked on
+              high-impact projects, optimizing data processing for large-scale transactions,
+              ensuring regulatory compliance, and migrating applications to the cloud. Plus, I’ve
+              engineered solutions that significantly boosted performance and scalability.
+            </p>
+
+            <p>
+              Outside of my day job, I’m always exploring new technologies and sharing my knowledge.
+              I recently developed{' '}
+              <a href="https://github.com/Kelechiibekwe/microservices">
+                a microservices-based Fraud Detector app
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              using Spring Boot, Docker, RabbitMQ, and Kafka, showcasing my ability to build
+              scalable, efficient systems.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
